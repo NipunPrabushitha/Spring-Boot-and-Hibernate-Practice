1b36b353e00c3b5699115fe6257ddad162f650a9
User <nprabushitha@gmail.com>
2025-11-12 16:57:36 +0530

    done save one student and multiple students

--- cruddemo/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ cruddemo/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.cruddemo;
 
+import com.example.cruddemo.dao.StudentDAO;
+import com.example.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,34 @@ public class CruddemoApplication {
 	}
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner->{
-            System.out.println("Hello World");
+            //createStudent(studentDAO);
+
+            createMultipleStudents(studentDAO);
         };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+        System.out.println("Creating 3 new student objects");
+        Student tempStudent1 = new Student("Shashi","Madhushan","shashi@gmail.com");
+        Student tempStudent2 = new Student("Apeksha","Vidanage","apeksha@gmail.com");
+        Student tempStudent3 = new Student("Udara","Sankalpa","udara@gmail.com");
+
+        System.out.println("Saving 3 new student objects");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("Creating the new student");
+        Student tempStudent = new Student("Nipun","Prabushitha","nprabushitha@gmail.com");
+
+        System.out.println("Saving the student");
+        studentDAO.save(tempStudent);
+
+        System.out.println("Saved student. Generate id : " +  tempStudent.getId());
     }
 }
