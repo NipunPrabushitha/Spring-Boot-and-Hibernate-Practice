c295568e06878edd3b6794729e1b2e6d94372aef
User <nprabushitha@gmail.com>
2025-11-13 08:29:56 +0530

    get list of Students

---
+++ demo/demo/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.rest;
+
+import com.example.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Nipun","Prabushitha"));
+        students.add(new Student("Apeksha","Vidanage"));
+        students.add(new Student("Pathum","Sankalpa"));
+        return students;
+    }
+}
