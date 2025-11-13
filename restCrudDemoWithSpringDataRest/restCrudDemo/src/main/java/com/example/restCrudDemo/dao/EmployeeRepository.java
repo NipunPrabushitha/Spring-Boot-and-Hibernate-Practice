222bcf7da2693f92c9d5c8e9baf50343b9f17460
User <nprabushitha@gmail.com>
2025-11-13 16:37:19 +0530

    add page size

--- restCrudDemoWithSpringDataRest/restCrudDemo/src/main/java/com/example/restCrudDemo/dao/EmployeeRepository.java
+++ restCrudDemoWithSpringDataRest/restCrudDemo/src/main/java/com/example/restCrudDemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.restCrudDemo.dao;
 
 import com.example.restCrudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
