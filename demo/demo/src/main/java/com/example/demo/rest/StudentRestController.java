14ff562cdc9272be9e1cef85d86e06fc5895f709
User <nprabushitha@gmail.com>
2025-11-13 08:33:59 +0530

    done path variable approach

--- demo/demo/src/main/java/com/example/demo/rest/StudentRestController.java
+++ demo/demo/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+         students = new ArrayList<>();
         students.add(new Student("Nipun","Prabushitha"));
         students.add(new Student("Apeksha","Vidanage"));
         students.add(new Student("Pathum","Sankalpa"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
         return students;
     }
 }
