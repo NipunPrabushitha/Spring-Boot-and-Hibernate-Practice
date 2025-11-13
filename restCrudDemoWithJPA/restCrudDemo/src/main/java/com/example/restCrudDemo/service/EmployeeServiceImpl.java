fa2daf459a0e2c93fe5918ea04ca4cc95d117c5c
User <nprabushitha@gmail.com>
2025-11-13 13:21:21 +0530

    update project for JPA

---
+++ restCrudDemoWithJPA/restCrudDemo/src/main/java/com/example/restCrudDemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.restCrudDemo.service;
+
+import com.example.restCrudDemo.dao.EmployeeDAO;
+import com.example.restCrudDemo.entity.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+       return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
