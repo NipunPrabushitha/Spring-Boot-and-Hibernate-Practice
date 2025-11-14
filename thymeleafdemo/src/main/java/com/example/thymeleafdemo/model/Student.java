66e471d048a3487ac053f4563ac803da606114f5
User <nprabushitha@gmail.com>
2025-11-14 09:33:03 +0530

    Mvc drop down data binding

--- thymeleafdemo/src/main/java/com/example/thymeleafdemo/model/Student.java
+++ thymeleafdemo/src/main/java/com/example/thymeleafdemo/model/Student.java
@@ -3,14 +3,16 @@ package com.example.thymeleafdemo.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student() {
 
     }
 
-    public Student(String firstName, String lastName) {
+    public Student(String firstName, String lastName, String country) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.country = country;
     }
 
     public String getFirstName() {
@@ -29,11 +31,20 @@ public class Student {
         this.lastName = lastName;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
                 '}';
     }
 }
